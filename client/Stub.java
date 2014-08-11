85f500db7a4f643772c653d5b01d771a330c6d49
deviouspk <aamining2@gmail.com>
2014-08-11 20:19:17 +0200

    upload code

---
+++ client/Stub.java
@@ -0,0 +1,41 @@
+package loader.client;
+
+import java.applet.AppletContext;
+import java.applet.AppletStub;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Stub implements AppletStub {
+	@Override
+	public void appletResize(int width, int height) {
+		System.out.println("New Size: '"+width+", "+height+"'");
+	}
+
+	@Override
+	public AppletContext getAppletContext() {
+		return null;
+	}
+
+	@Override
+	public URL getCodeBase() {
+		try {
+			return new URL("http://deviouspk.com/");
+		} catch (MalformedURLException ignore) {}
+		return null;
+	}
+
+	@Override
+	public URL getDocumentBase() {
+		return getCodeBase();
+	}
+
+	@Override
+	public String getParameter(String name) {
+		return Client.PARAMETERS.get(name);
+	}
+
+	@Override
+	public boolean isActive() {
+		return true;
+	}
+}
